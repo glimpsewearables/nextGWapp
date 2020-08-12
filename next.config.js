a4f03664519892776c714265f8c4b4c179ca321d
glimspewearables <drose@glimspewearables.com>
2020-08-12 01:19:59 -0700

    fixed script path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	env: {
-		baseURL: 'http://192.168.50.5:3000',
+		baseURL: 'http://192.168.86.234:3000',
 		pythonPath: '/usr/bin/python', //path where python is installed
-		scriptDir: '/home/pi/backend/nextGW/Autohotspot/' // make sure you use an absolute path for scriptPath
+		scriptDir: '/home/pi/nextGWapp/Autohotspot/' // make sure you use an absolute path for scriptPath
 	}
 }
